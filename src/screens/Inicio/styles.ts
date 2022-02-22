64722c92a45e035fc767b5109b69ee47661147a0
Matheus Vitor Sousa Almeida <df90342ps@df.trf1.gov.br>
2022-02-22 14:39:33 -0300

    Layout

---
+++ src/screens/Inicio/styles.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+
+    /* background-color: #141414; */
+`;
+
+export const Texto = styled.Text`
+    color: #fff;
+`;
+
+export const ButtonInit = styled.Button`
+    position: absolute;
+    display: flex;
+
+    margin: 150px;
+
+    width: 100px;
+    height: 50px;
+
+`;
+
+export const Content = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: space-evenly;
+    flex-direction: row;
+
+    position: absolute;
+
+    left: 20px;
+    bottom: 20px
+
+    width: 100%;
+    /* background-color: #141414; */
+
+`;  
+
